b847d12b71eed38e8f65a2ca96b04415726f9e24
Maryam EL-ganainy <mrimhassn18@gmail.com>
2026-08-30 01:30:24 +0300

    Add files via upload

---
+++ EF Core/Assignment2 (EventHub) /Configurations/RegistrationConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using EFCore2.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore2.Configurations
+{
+    public class RegistrationConfiguration : IEntityTypeConfiguration<Registration>
+    {
+        public void Configure(EntityTypeBuilder<Registration> builder)
+        {
+
+            // configure the many-to-many relationship between Event and Attendee through Registration
+            builder
+                .HasKey(r => new { r.EventId, r.AttendeeId });
+
+            builder
+                .HasOne(r => r.Event)
+                .WithMany(e => e.Registrations)
+                .HasForeignKey(r => r.EventId);
+
+            builder
+                .HasOne(r => r.Attendee)
+                .WithMany(a => a.Registrations)
+                .HasForeignKey(r => r.AttendeeId);
+        }
+    }
+}
