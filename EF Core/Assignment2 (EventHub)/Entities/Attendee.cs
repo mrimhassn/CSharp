c3ef6e08f214c44fa628e63a313950358938ab27
Maryam EL-ganainy <mrimhassn18@gmail.com>
2026-08-30 01:32:26 +0300

    Add files via upload

---
+++ EF Core/Assignment2 (EventHub)/Entities/Attendee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCore2.Entities
+{
+    public class Attendee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+        public string Street { get; set; } = null!;
+        public string City { get; set; } = null!;
+
+        [Required]
+        public string Country { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+
+        // navigation property 
+        // one to one relationship between Attendee and Badge
+        public Badge? Badge { get; set; } = null!; // null cause can be zero or one badge for attendee
+
+        // 1 to many relationship (between Attendee and Registration)
+        public ICollection<Registration> Registrations { get; set; } = [];
+
+    }
+}
---
+++ EF Core/Assignment2 (EventHub)/Entities/Organizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCore2.Entities
+{
+    public class Organizer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+        public string? NameOfCompany { get; set; }
+        public  bool IsVerified { get; set; }
+
+        //  navigation property 
+        // one to one relationship between Organizer and PublicFacingProfile
+        public PublicFacingProfile PublicFacingProfile { get; set; } = null!;
+
+        // 1 to many relationship (between organizer and Event)
+        public ICollection<Event> Events { get; set; } = [];
+
+    }
+}
